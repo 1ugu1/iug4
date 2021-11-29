7b86b76f5c16d21931eda86cb408f38b5ab295f4
1ugu1 <92942602+1ugu1@users.noreply.github.com>
2021-11-29 12:01:35 -0300

    Create iug4.sh

---
+++ iug4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo "~~~~~~~~~WELCOME TO IUG3~~~~~~~~~~"
+echo "~                                ~"
+echo "~        Powered by: 1ugu1       ~"
+echo "~                                ~"
+echo "~ this tool will do a port scan  ~"
+echo "~                                ~"
+echo "~                                ~"
+echo "~                                ~"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+if [ "$1" == "" ] 
+
+then
+        echo "Type the goal link"
+
+else 
+for port in {1..10000}
+do
+hping3 -S -p $port -c 1 $1  2> /dev/null | grep "flags=SA"| cut -d " " -f 5 | cut -d "=" -f2 
+done
+fi
